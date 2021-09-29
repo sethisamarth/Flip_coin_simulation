206cc19c904f2b6c192a0bdc7db753895c08f9e4
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-09-29 17:45:05 +0530

    if total head = total tail

--- toss.sh
+++ toss.sh
@@ -2,6 +2,9 @@
 total_head=0
 total_tail=0
 
+new_head=0
+new_tail=0
+
 while (( total_head<21 && total_tail<21 ))
 do
 toss=$((RANDOM%2))
@@ -34,7 +37,25 @@ else
 fi
 
 
+if ((  $total_head == $total_tail ))
+then
+    while ((  (( $new_head - $new_tail )) <= 2 && (( $new_tail - $new_head )) <= 2  ))
+    do
+toss=$((RANDOM%2))
 
 
+
+if (( $toss == 0 ))
+then
+   echo "its head"
+      (( new_head++ ))
+else
+    echo "its tail"
+      (( new_tail++ ))
+fi
+
+done
+
+fi
 
 
