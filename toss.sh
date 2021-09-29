3c1724eaccb4ca5fdd7bd74469032fea48339b00
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-09-29 15:41:29 +0530

    multiple toss

--- toss.sh
+++ toss.sh
@@ -1,8 +1,31 @@
-#!/bin/bash
-toss=$(( RANDOM % 2 ))
-if(( toss == 0 ))
+#! /bin/bash -x
+total_head=0
+total_tail=0
+read -p "Enter the how many times you want to toss:n" n
+for (( i=1; i<=n; i++))
+do
+toss=$((RANDOM%2))
+
+
+
+if (( $toss == 0 ))
 then
-    echo "Heads"
+   echo "its head"
+      (( total_head++ ))
 else
-    echo "Tails"
+    echo "its tail"
+      (( total_tail++ ))
 fi
+
+done
+echo "$total_head"
+echo "$total_tail"
+
+if(( $total_head > $total_tail ))
+then
+      echo "winner is heads"
+else
+      echo "winner is tails"
+fi
+
+
